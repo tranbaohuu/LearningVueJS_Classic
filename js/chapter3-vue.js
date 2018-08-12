5bb4e9beea39495d7135c50fbd42c5456a6dcd93
Tran Bao Huu <tranbaohuu@gmail.com>
2018-08-13 00:04:59 +0700

    +Học hết chương 3

---
+++ js/chapter3-vue.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: "#obj",
+    data: {
+        show: true,
+
+        //khai báo 1 mảng array
+
+        ingredients: ['meat', 'fruit', 'cookies'],
+
+        //kiểu mảng phần tử object
+
+        persons: [
+            {name: 'Max', age: 27, color: 'red'},
+            {name: 'Anna', age: 'unknown', color: 'blue'}
+        ]
+
+
+    }
+
+});
