1a60cb06350a32b0693f74ba5b4580a049b2ff60
Tran Bao Huu <tranbaohuu@gmail.com>
2018-08-17 00:34:49 +0700

    +Học hết chương 6, làm quen với VueCLI 2. Version mới nhất là VueCLI 3 (nhưng ko dùng tại đây)

---
+++ VueCLI_Demo/my-vue-cli-2/README.md
@@ -0,0 +1,18 @@
+# my-vue-cli-2
+
+> A demo build with vue-cli-2. Latest is vue-cli-3
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ VueCLI_Demo/my-vue-cli-2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <!--lưu ý trong template chi có 1 div và tất cả phải viết trong đó nếu không sẽ biên dịch lỗi-->
+    <!--<div><h1>Hello World ! {{msg}}</h1></div>-->
+
+    <div><h1>Hello World !</h1></div>
+
+    <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+    <!--<h1>{{ msg }}</h1>-->
+    <!--<h2>Essential Links</h2>-->
+    <!--<ul>-->
+    <!--<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>-->
+    <!--<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>-->
+    <!--<li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>-->
+    <!--<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>-->
+    <!--</ul>-->
+    <!--<h2>Ecosystem</h2>-->
+    <!--<ul>-->
+    <!--<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>-->
+    <!--<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>-->
+    <!--<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>-->
+    <!--<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>-->
+    <!--</ul>-->
+    <!--</div>-->
+</template>
+
+<script>
+    export default {
+        // name: 'app',
+        // data() {
+        //     return {
+        //         msg: 'Welcome to Your Vue.js App'
+        //     }
+        // }
+    }
+</script>
+
+<style>
+
+</style>
