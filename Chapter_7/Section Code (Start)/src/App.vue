01f3346a72ac86d354b13261079905626865fc91
Tran Bao Huu <tranbaohuu@gmail.com>
2018-08-19 23:08:51 +0700

    Học đến bài 91 chương 7

---
+++ Chapter_7/Section Code (Start)/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Server Status: {{status}}</h1>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "my-cm",
+        data: function () {
+            return {
+                status: "My Critical Status"
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
